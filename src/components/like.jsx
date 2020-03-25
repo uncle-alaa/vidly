7ab752519695bfbc6c26f6a541d4294486ac4e68
ABOURIZK <ahmed.abourizk@capgemini.com>
2020-03-25 17:24:47 +0100

    supp all content

--- src/components/like.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Like = (props) => {
-    let classes = "fa fa-heart";
-    if (!props.liked) classes += "-o"
-    return (<i style={{ cursor: 'pointer' }} onClick={props.onClick} className={classes} ></i>);
-}
-
-export default Like;
